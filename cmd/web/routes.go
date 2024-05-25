9b8b183eb362a20dd284cff110a2a7aadbaebff9
santiagobedoa <santiagobedoa@gmail.com>
2024-05-24 21:32:05 -0500

    chapter 6.3

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -2,13 +2,16 @@ package main
 
 import "net/http"
 
-// The routes() method returns a servemux containing our application routes.
-func (app *application) routes() *http.ServeMux {
+// Update the signature for the routes() method so that it returns a
+// http.Handler instead of *http.ServeMux.
+func (app *application) routes() http.Handler {
 	mux := http.NewServeMux()
 	fileServer := http.FileServer(http.Dir("./ui/static/"))
 	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
 	mux.HandleFunc("/", app.home)
 	mux.HandleFunc("/snippet/view", app.snippetView)
 	mux.HandleFunc("/snippet/create", app.snippetCreate)
-	return mux
+	// Pass the servemux as the 'next' parameter to the secureHeaders middleware. // Because secureHeaders is just a function, and the function returns a
+	// http.Handler we don't need to do anything else.
+	return secureHeaders(mux)
 }
