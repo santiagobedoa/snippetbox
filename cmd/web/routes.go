2577d214ec45b90c6605545c35772e8b394dead0
santiagobedoa <santiagobedoa@gmail.com>
2024-05-27 20:49:20 -0500

    chapter 6.4

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -6,12 +6,14 @@ import "net/http"
 // http.Handler instead of *http.ServeMux.
 func (app *application) routes() http.Handler {
 	mux := http.NewServeMux()
+
 	fileServer := http.FileServer(http.Dir("./ui/static/"))
 	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
+
 	mux.HandleFunc("/", app.home)
 	mux.HandleFunc("/snippet/view", app.snippetView)
 	mux.HandleFunc("/snippet/create", app.snippetCreate)
-	// Pass the servemux as the 'next' parameter to the secureHeaders middleware. // Because secureHeaders is just a function, and the function returns a
-	// http.Handler we don't need to do anything else.
-	return secureHeaders(mux)
+
+	// Wrap the existing chain with the logRequest middleware.
+	return app.recoverPanic(app.logRequest(secureHeaders(mux)))
 }
