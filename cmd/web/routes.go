8d4714c9d8d7bd29559a74fbeef15b868c396393
santiagobedoa <santiagobedoa@gmail.com>
2024-05-29 16:28:14 -0500

    chapter 7.1

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -1,19 +1,21 @@
 package main
 
-import "net/http"
+import (
+	"net/http"
 
-// Update the signature for the routes() method so that it returns a
-// http.Handler instead of *http.ServeMux.
+	"github.com/justinas/alice" // New import
+)
+
 func (app *application) routes() http.Handler {
 	mux := http.NewServeMux()
-
 	fileServer := http.FileServer(http.Dir("./ui/static/"))
 	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
-
 	mux.HandleFunc("/", app.home)
 	mux.HandleFunc("/snippet/view", app.snippetView)
 	mux.HandleFunc("/snippet/create", app.snippetCreate)
-
-	// Wrap the existing chain with the logRequest middleware.
-	return app.recoverPanic(app.logRequest(secureHeaders(mux)))
+	// Create a middleware chain containing our 'standard' middleware
+	// which will be used for every request our application receives.
+	standard := alice.New(app.recoverPanic, app.logRequest, secureHeaders)
+	// Return the 'standard' middleware chain followed by the servemux.
+	return standard.Then(mux)
 }
