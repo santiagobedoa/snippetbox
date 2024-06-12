3dd33f6465be5404cefb426ce6f024d8d7f58133
santiagobedoa <santiagobedoa@gmail.com>
2024-06-12 09:39:57 -0500

    chapter 11

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -89,6 +89,10 @@ func main() {
 		ErrorLog:  errorLog,
 		Handler:   app.routes(),
 		TLSConfig: tlsConfig,
+		// Add Idle, Read and Write timeouts to the server.
+		IdleTimeout:  time.Minute,
+		ReadTimeout:  5 * time.Second,
+		WriteTimeout: 10 * time.Second,
 	}
 
 	infoLog.Printf("Starting server on %s", *addr)
