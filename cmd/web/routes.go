9e772104324fa9352846cd67a985eb4a2351b933
santiagobedoa <santiagobedoa@gmail.com>
2024-07-01 17:47:46 -0500

    chapter 14.4

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -14,18 +14,12 @@ func (app *application) routes() http.Handler {
 		app.notFound(w)
 	})
 
-	// Take the ui.Files embedded filesystem and convert it to a http.FS type so
-	// that it satisfies the http.FileSystem interface. We then pass that to the
-	// http.FileServer() function to create the file server handler.
 	fileServer := http.FileServer(http.FS(ui.Files))
 
-	// Our static files are contained in the "static" folder of the ui.Files
-	// embedded filesystem. So, for example, our CSS stylesheet is located at
-	// "static/css/main.css". This means that we now longer need to strip the
-	// prefix from the request URL -- any requests that start with /static/ can
-	// just be passed directly to the file server and the corresponding static
-	// file will be served (so long as it exists).
 	router.Handler(http.MethodGet, "/static/*filepath", fileServer)
+
+	// GET /ping route just for testing
+	router.HandlerFunc(http.MethodGet, "/ping", ping)
 
 	// Add the authenticate() middleware to the chain.
 	dynamic := alice.New(app.sessionManager.LoadAndSave, noSurf, app.authenticate)
